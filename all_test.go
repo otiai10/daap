3d9b7cdf5271ccde931513f915bc2f25ccee5212
Hiromu OCHIAI <otiai10@gmail.com>
2017-12-11 18:36:55 +0900

    Setup travis

--- all_test.go
+++ all_test.go
@@ -1,3 +1,26 @@
 package daap
 
-// import . "github.com/otiai10/mint"
+import (
+	"context"
+	"testing"
+
+	. "github.com/otiai10/mint"
+)
+
+func TestNewContainer(t *testing.T) {
+	container := NewContainer("debian:latest", Args{
+		Machine: NewEnvMachine(),
+	})
+	Expect(t, container).TypeOf("*daap.Container")
+}
+
+func TestContainer_PullImage(t *testing.T) {
+	container := NewContainer("debian:latest", Args{
+		Machine: NewEnvMachine(),
+	})
+	pulllog, err := container.PullImage(context.Background())
+	Expect(t, err).ToBe(nil)
+	for log := range pulllog {
+		Expect(t, log).TypeOf("daap.ImagePullResponsePayload")
+	}
+}
