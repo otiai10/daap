694df516db1306f93be14d87896eef03932f942d
Hiromu OCHIAI <otiai10@gmail.com>
2017-12-11 15:02:54 +0900

    Fix streaming from Exec

--- container.go
+++ container.go
@@ -90,11 +90,11 @@ func (c *Container) Start(ctx context.Context) error {
 
 // Exec executes specified command on this container.
 // Before calling "Exec", this container must be created and started.
-func (c *Container) Exec(ctx context.Context, out chan<- []byte, cmd ...string) error {
+func (c *Container) Exec(ctx context.Context, cmd ...string) (<-chan HijackedPayload, error) {
 
 	dkclient, err := c.Args.Machine.CreateClient()
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer dkclient.Close()
 
@@ -104,41 +104,34 @@ func (c *Container) Exec(ctx context.Context, out chan<- []byte, cmd ...string) 
 		AttachStderr: true,
 	})
 	if err != nil {
-		return fmt.Errorf("Exec Create Error: %v", err)
+		return nil, fmt.Errorf("Exec Create Error: %v", err)
 	}
 
 	hijacked, err := dkclient.ContainerExecAttach(ctx, execute.ID, types.ExecConfig{})
 	if err != nil {
-		return fmt.Errorf("Exec Attach Error: %v", err)
+		return nil, fmt.Errorf("Exec Attach Error: %v", err)
 	}
 	// defer hijacked.Close()
 
 	err = dkclient.ContainerExecStart(ctx, execute.ID, types.ExecStartCheck{})
 	if err != nil {
 		hijacked.Close()
-		return fmt.Errorf("Exec Start Error: %v", err)
+		return nil, fmt.Errorf("Exec Start Error: %v", err)
 	}
 
+	stream := make(chan HijackedPayload)
 	scanner := bufio.NewScanner(hijacked.Reader)
-
-	// If out is not given, it executes syncly.
-	if out == nil {
-		for scanner.Scan() {
-			// TODO: Pool output somewhere
-			fmt.Println(scanner.Text())
-		}
-		hijacked.Close()
-		return scanner.Err()
-	}
-
-	// If out is given, delegate output to the caller.
 	go func() {
+		defer close(stream)
+		defer hijacked.Close()
 		for scanner.Scan() {
-			out <- scanner.Bytes()
+			stream <- HijackedPayload{
+				Type: MIXED,
+				Data: scanner.Bytes(),
+			}
 		}
-		hijacked.Close()
-		close(out)
+		return
 	}()
 
-	return nil
+	return stream, nil
 }
