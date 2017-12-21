c12d1ce4ce3381f7b359b270f596a1fd6495d661
Hiromu OCHIAI <otiai10@gmail.com>
2017-12-21 18:55:37 +0900

    Mount

--- container.go
+++ container.go
@@ -66,6 +66,11 @@ func (c *Container) Create(ctx context.Context) error {
 		return err
 	}
 	defer dkclient.Close()
+
+	mounts := []mount.Mount{}
+	for _, m := range c.Args.Mounts {
+		mounts = append(mounts, m.ToDockerAPITypeMount())
+	}
 	c.ContainerCreateCreatedBody, err = dkclient.ContainerCreate(
 		ctx,
 		&container.Config{
@@ -76,7 +81,7 @@ func (c *Container) Create(ctx context.Context) error {
 			AttachStderr: true,
 		},
 		&container.HostConfig{
-			Mounts: []mount.Mount{}, // e.g. VolumesFrom
+			Mounts: mounts,
 		},
 		&network.NetworkingConfig{},
 		c.Args.Name,
@@ -154,56 +159,4 @@ func (c *Container) genExecCommand(ctx context.Context, execution Execution) ([]
 	//       It might be determined by extension of filename.
 	cmd := []string{"sh", "/" + filepath.Base(script.Name())}
 	return cmd, nil
-}
-
-// Exec executes specified command on this container.
-// Before calling "Exec", this container must be created and started.
-func (c *Container) Exec(ctx context.Context, execution Execution) (<-chan HijackedStreamPayload, error) {
-
-	dkclient, err := c.Args.Machine.CreateClient()
-	if err != nil {
-		return nil, err
-	}
-	defer dkclient.Close()
-
-	cmd, err := c.genExecCommand(ctx, execution)
-	if err != nil {
-		return nil, err
-	}
-
-	execute, err := dkclient.ContainerExecCreate(ctx, c.ID, types.ExecConfig{
-		Cmd:          cmd,
-		AttachStdout: true,
-		AttachStderr: true,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("Exec Create Error: %v", err)
-	}
-
-	hijacked, err := dkclient.ContainerExecAttach(ctx, execute.ID, types.ExecStartCheck{})
-	if err != nil {
-		return nil, fmt.Errorf("Exec Attach Error: %v", err)
-	}
-	// defer hijacked.Close()
-
-	err = dkclient.ContainerExecStart(ctx, execute.ID, types.ExecStartCheck{})
-	if err != nil {
-		hijacked.Close()
-		return nil, fmt.Errorf("Exec Start Error: %v", err)
-	}
-
-	stream := make(chan HijackedStreamPayload)
-	scanner := bufio.NewScanner(hijacked.Reader)
-	go func() {
-		defer close(stream)
-		defer hijacked.Close()
-		payload := HijackedStreamPayload{Type: MIXED}
-		for scanner.Scan() {
-			// If raw bytes doesn't have header, use previous io type as a default.
-			payload = CreatePayloadFromRawBytes(payload.Type, scanner.Bytes())
-			stream <- payload
-		}
-		return
-	}()
-	return stream, nil
 }
--- exec.go
+++ exec.go
@@ -1,7 +1,69 @@
 package daap
 
+import (
+	"bufio"
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+)
+
 // Execution ...
 type Execution struct {
 	Inline string
 	Script string
+	Env    []string
+}
+
+// Exec executes specified command on this container.
+// Before calling "Exec", this container must be created and started.
+func (c *Container) Exec(ctx context.Context, execution Execution) (<-chan HijackedStreamPayload, error) {
+
+	dkclient, err := c.Args.Machine.CreateClient()
+	if err != nil {
+		return nil, err
+	}
+	defer dkclient.Close()
+
+	cmd, err := c.genExecCommand(ctx, execution)
+	if err != nil {
+		return nil, err
+	}
+
+	execute, err := dkclient.ContainerExecCreate(ctx, c.ID, types.ExecConfig{
+		Cmd:          cmd,
+		AttachStdout: true,
+		AttachStderr: true,
+		Env:          execution.Env,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Exec Create Error: %v", err)
+	}
+
+	hijacked, err := dkclient.ContainerExecAttach(ctx, execute.ID, types.ExecStartCheck{})
+	if err != nil {
+		return nil, fmt.Errorf("Exec Attach Error: %v", err)
+	}
+	// defer hijacked.Close()
+
+	err = dkclient.ContainerExecStart(ctx, execute.ID, types.ExecStartCheck{})
+	if err != nil {
+		hijacked.Close()
+		return nil, fmt.Errorf("Exec Start Error: %v", err)
+	}
+
+	stream := make(chan HijackedStreamPayload)
+	scanner := bufio.NewScanner(hijacked.Reader)
+	go func() {
+		defer close(stream)
+		defer hijacked.Close()
+		payload := HijackedStreamPayload{Type: MIXED}
+		for scanner.Scan() {
+			// If raw bytes doesn't have header, use previous io type as a default.
+			payload = CreatePayloadFromRawBytes(payload.Type, scanner.Bytes())
+			stream <- payload
+		}
+		return
+	}()
+	return stream, nil
 }
