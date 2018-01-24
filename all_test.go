6b65a3a37d36a73bd00bdc838330bf8dfef4d1d4
Hiromu OCHIAI <otiai10@gmail.com>
2018-01-24 12:07:28 +0900

    Add prototype of testing

--- all_test.go
+++ all_test.go
@@ -1,26 +1,38 @@
 package daap
 
 import (
-	"context"
+	"fmt"
+	"os"
 	"testing"
 
+	"github.com/otiai10/dkmachine/v0/dkmachine"
 	. "github.com/otiai10/mint"
+	"github.com/otiai10/ternary"
 )
 
-func TestNewContainer(t *testing.T) {
-	container := NewContainer("debian:latest", Args{
-		Machine: NewEnvMachine(),
+var testmachine *MachineConfig
+
+func TestMain(m *testing.M) {
+	machine, err := dkmachine.Create(&dkmachine.CreateOptions{
+		Driver: ternary.String(os.Getenv("DRIVER"))("virtualbox"),
 	})
-	Expect(t, container).TypeOf("*daap.Container")
+	if err != nil {
+		panic(err)
+	}
+	testmachine = &MachineConfig{
+		Host:     machine.Host(),
+		CertPath: machine.CertPath(),
+	}
+	code := m.Run()
+	if err := machine.Remove(); err != nil {
+		panic(err)
+	}
+	os.Exit(code)
 }
 
-func TestContainer_PullImage(t *testing.T) {
-	container := NewContainer("debian:latest", Args{
-		Machine: NewEnvMachine(),
-	})
-	pulllog, err := container.PullImage(context.Background())
-	Expect(t, err).ToBe(nil)
-	for log := range pulllog {
-		Expect(t, log).TypeOf("daap.ImagePullResponsePayload")
-	}
+func TestNewContainer(t *testing.T) {
+	container := NewContainer("debian:latest", Args{Machine: testmachine})
+	Expect(t, container).TypeOf("*daap.Container")
+	fmt.Printf("Container: %+v\n", container)
+	fmt.Printf("Machine: %+v\n", container.Args.Machine)
 }
