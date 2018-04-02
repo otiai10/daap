bbe1fa37e9ef6a47a58a58604255b99b874acae1
Hiromu OCHIAI <otiai10@gmail.com>
2018-04-02 17:32:58 +0900

    Change Volume interface

--- mount.go
+++ mount.go
@@ -2,9 +2,9 @@ package daap
 
 import "github.com/docker/docker/api/types/mount"
 
-// MountVolume represents --volume source:target,
+// Bind represents --volume source:target,
 // shorthand function to create "types/mount".Mount
-func MountVolume(source, target string, readonly ...bool) mount.Mount {
+func Bind(source, target string, readonly ...bool) mount.Mount {
 	readonly = append(readonly, false)
 	return mount.Mount{
 		Type:     mount.TypeBind,
@@ -14,11 +14,22 @@ func MountVolume(source, target string, readonly ...bool) mount.Mount {
 	}
 }
 
-// MountVolumesFrom represents --volumes-from containerID,
+// VolumesFrom represents --volumes-from containerID,
 // shorthand function to create "types/mount".Mount
-func MountVolumesFrom(containerID string) mount.Mount {
+func VolumesFrom(containerID string) mount.Mount {
 	return mount.Mount{
 		Type:   mount.TypeVolume,
 		Source: containerID,
+	}
+}
+
+// VolumeByName ...
+func VolumeByName(volumename, target string, readonly ...bool) mount.Mount {
+	readonly = append(readonly, false)
+	return mount.Mount{
+		Type:     mount.TypeVolume,
+		Source:   volumename,
+		Target:   target,
+		ReadOnly: readonly[0],
 	}
 }
